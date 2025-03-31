fcad7d1bb851d9b045a837a1d486779c9bbd36a0
NatC02 <natanceballos02@gmail.com>
2025-03-31 06:27:19 -0400

    Scene, Camera, and Renderer Setup

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ import { randomShader } from './shaders/randomShader.js';
 import { fallenRoseShader } from './shaders/fallenRoseShader.js';
 import { mazeShader } from './shaders/mazeShader.js';
 import { rainShader } from './shaders/rainShader.js';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#394648');
+
+const camera = new THREE.PerspectiveCamera(30, innerWidth/innerHeight);
+camera.position.set(0, 0, 7);
+camera.lookAt(scene.position);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Commit 3: Resize and Controls Setup
+window.addEventListener("resize", (event) => {
+    camera.aspect = innerWidth/innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(innerWidth, innerHeight);
+    
+    // Update shader uniforms with new resolution
+    updateShaderUniforms();
+});
