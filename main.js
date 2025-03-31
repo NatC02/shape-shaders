7e94ff4f693835727e66c7bbdfa44f096277a2ed
NatC02 <natanceballos02@gmail.com>
2025-03-31 06:26:44 -0400

    setup and imports

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { everflowShader } from './shaders/everflowShader.js';
+import { pulseShader } from './shaders/pulseShader.js';
+import { randomShader } from './shaders/randomShader.js';
+import { fallenRoseShader } from './shaders/fallenRoseShader.js';
+import { mazeShader } from './shaders/mazeShader.js';
+import { rainShader } from './shaders/rainShader.js';
